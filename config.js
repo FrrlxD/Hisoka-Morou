777914907afd3d7baf8ff9a434f754de65a9a717
FrrlxD <105418953+FrrlxD@users.noreply.github.com>
2022-10-04 08:10:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6281261436896','62895634841968','6282298463886','6281261436896']
+global.premium = ['6281261436896']
+global.packname = 'FrrlDfni'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
